7cabd2c28dce137b43638e3e8cdc59dcab6ce107
marcvillareal <marcvillareal30@gmail.com>
2025-09-22 09:58:59 +0100

    Setting up backend api

---
+++ sync-fit-api/src/main/java/com/syncfit/SyncFitApplication.java
@@ -0,0 +1,13 @@
+package com.syncfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncFitApplication.class, args);
+    }
+
+}
